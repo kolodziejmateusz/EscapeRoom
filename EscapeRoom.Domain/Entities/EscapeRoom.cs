5abaac24c9d9608423830cea2ed098965448207a
Mateusz Kolodziej <mateuszkolodziejti@gmail.com>
2024-03-14 12:46:41 +0100

    model EscapeRoom

---
+++ EscapeRoom.Domain/Entities/EscapeRoom.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EscapeRoom.Domain.Entities
+{
+    public class EscapeRoom
+    {
+        public required int Id { get; set; }
+        public string Name { get; set; } = default!;
+        public string? Description { get; set; }
+        public DateTime CratedAt { get; set; } = DateTime.UtcNow;
+        public EscapeRoomAddressDetails AddressDetails { get; set; } = default!;
+
+        public string EncodedName { get; private set; } = default!;
+
+        public void EncodeName()
+        {
+            EncodedName = Name.ToLower().Replace(" ", "-");
+        }
+    }
+}
---
+++ EscapeRoom.Domain/Entities/EscapeRoomAddressDetails.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EscapeRoom.Domain.Entities
+{
+    public class EscapeRoomAddressDetails
+    {
+        public string? PhoneNumber { get; set; }
+        public string? Street { get; set; }
+        public string? City { get; set; }
+        public string? PostalCode { get; set; }
+        
+    }
+}
