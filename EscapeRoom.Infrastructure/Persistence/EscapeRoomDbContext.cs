05e540c04081faa6044197e87bb3f37c15336975
Mateusz Kolodziej <mateuszkolodziejti@gmail.com>
2024-03-14 14:12:48 +0100

    DbContext i baza danych

---
+++ EscapeRoom.Infrastructure/Persistence/EscapeRoomDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EscapeRoom.Infrastructure.Persistence
+{
+    public class EscapeRoomDbContext : DbContext
+    {
+        public DbSet<Domain.Entities.EscapeRoom> EscapeRooms { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=EscapeRoomDb;Trusted_Connection=True");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Domain.Entities.EscapeRoom>()
+                .OwnsOne(c => c.AddressDetails);
+        }
+    }
+}
