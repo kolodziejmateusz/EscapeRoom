d4b2156c81b5f97faf1682492f31fc3a7e30fa61
Mateusz Kolodziej <mateuszkolodziejti@gmail.com>
2024-03-25 14:52:04 +0100

    Wyświetlanie recenzji na widoku edycji i szczegółów

--- EscapeRoom.MVC/wwwroot/js/site.js
+++ EscapeRoom.MVC/wwwroot/js/site.js
@@ -1,4 +1,54 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿const RenderEscapeRoomReviews = (reviews, container) => {
+    container.empty();
 
-// Write your JavaScript code.
+    for (const review of reviews) {
+        container.append(
+            `<div class="card border-secondary mb-3" style="max-width: 18rem; margin: 5px;">
+                <div class="card-header">${review.reviewerName}</div>
+                    <div class="card-body">
+                        <h6 class="card-title">${review.review}</h6> 
+                    </div>
+                </div>`
+        )
+    }
+}
+
+
+const LoadEscapeRoomReviews = () => {
+    const container = $("#reviews")
+    const escapeRoomEncodedName = container.data("encodedName");
+
+    $.ajax({
+        url: `/EscapeRoom/${escapeRoomEncodedName}/EscapeRoomReview`,
+        type: 'get',
+        success: function (data) {
+            if (!data.length) {
+                container.html("Ten Escape Room nie ma jeszcze recenzji")
+            } else {
+                RenderEscapeRoomReviews(data, container)
+            }
+        },
+        error: function () {
+            toastr["error"]("Cos poszlo nie tak")
+        }
+    })
+}
+
+const CreateEscapeRoomReview = () => {
+    $("#createEscapeRoomReviewModal form").submit(function (event) {
+        event.preventDefault();
+
+        $.ajax({
+            url: $(this).attr('action'),
+            type: $(this).attr('method'),
+            data: $(this).serialize(),
+            success: function (data) {
+                toastr["success"]("Utworzono recenzje")
+                LoadEscapeRoomReviews()
+            },
+            error: function () {
+                toastr["error"]("Cos poszlo nie tak, pusty formularz lub niezalogowany")
+            }
+        })
+    });
+}
