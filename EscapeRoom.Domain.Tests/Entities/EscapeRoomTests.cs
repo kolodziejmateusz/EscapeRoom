e67c705e91e00eab57ec22c8b08acb83e9495c1f
Mateusz Kolodziej <mateuszkolodziejti@gmail.com>
2024-04-03 11:26:02 +0200

    Utworzenie pustego projetu EscapeRoom.Domain.Tests

---
+++ EscapeRoom.Domain.Tests/Entities/EscapeRoomTests.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+using EscapeRoom.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EscapeRoom.Domain.Entities.Tests
+{
+    public class EscapeRoomTests
+    {
+        [Fact()]
+        public void EncodeNameTest()
+        {
+
+        }
+    }
+}
