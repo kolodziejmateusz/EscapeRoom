4e8ca44a7ba75842eec3d0ad6967a5e69bf33f15
Mateusz Kolodziej <mateuszkolodziejti@gmail.com>
2024-04-03 11:48:42 +0200

    EscapeRoom.Domain.Tests

--- EscapeRoom.Domain.Tests/Entities/EscapeRoomTests.cs
+++ EscapeRoom.Domain.Tests/Entities/EscapeRoomTests.cs
@@ -5,15 +5,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FluentAssertions;
 
 namespace EscapeRoom.Domain.Entities.Tests
 {
     public class EscapeRoomTests
     {
         [Fact()]
-        public void EncodeNameTest()
+        public void EncodeName_ShouldSetEncodeName()
         {
+            // arrange 
+            var escaperoom = new EscapeRoom();
+            escaperoom.Name = "   Bardzo Straszny Dom W Lesie    ";
 
+            // act
+            escaperoom.EncodeName();
+
+            // assert
+            escaperoom.EncodedName.Should().Be("bardzo-straszny-dom-w-lesie");
+        }
+
+        [Fact()]
+        public void EncodeName_ShouldThrowException_WhenIsNull()
+        {
+            // arrange 
+            var escaperoom = new EscapeRoom();
+
+            // act
+            Action action = () => escaperoom.EncodeName();
+
+            // assert
+            action.Invoking(a => a.Invoke())
+                .Should().Throw<NullReferenceException>();
         }
     }
 }
